e1b2d3df946573b030514c64410c134db31bd363
Enrique290 <enriquecastillocorona@gmail.com>
2026-02-07 09:03:37 -0600

    Final-Version-PageClosed

--- src/components/homePage/cv/cv.js
+++ src/components/homePage/cv/cv.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import CurriculumPDFEN from "../../../assets/pdf/homePage/CV-EN.pdf";
 import "./cv.css";
 
 export default function CV() {
@@ -8,7 +7,7 @@ export default function CV() {
     <div className="cv container-xl">
       <h1>Curriculum Vitae PDF</h1>
       <div className="cv-body">
-        <a href={CurriculumPDFEN} target="_blank" rel="noreferrer">
+        <a target="_blank" rel="noreferrer">
           <button type="button" className="btn">
             <i className="bi bi-download"> </i>
             Download
